a361a47338df906ef6f014959a156c2f4a4d6827
Arshiyan Ali <155827717+Arshiyan-ali@users.noreply.github.com>
2024-07-10 17:00:44 -0700

    Add files via upload

---
+++ search.dart
@@ -0,0 +1,11 @@
+void main(){
+  int key=3;
+  List <int> num=[1,2,5,4,3];
+  for (int i = 0; i < num.length;i++) {
+if (num[i]==key) {
+print('$key is found ');
+break;}
+
+  }
+}
+//output:3 is found
